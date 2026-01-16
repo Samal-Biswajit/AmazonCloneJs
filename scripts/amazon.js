4e60ea67fa5f66df2b3c902e962e00099d4dcad7
Biswajit Samal <biswajitsamalsibu03@gmail.com>
2026-01-17 03:01:56 +0530

    added checkout.js

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -84,7 +84,7 @@ document.querySelectorAll(".js-add-to-cart").forEach((button) => {
   button.addEventListener("click", () => {
     const { productId } = button.dataset;
     const productContainer = button.closest(".product-container");
-    
+
     addToCart(productId, productContainer);
     addedMessage(productContainer);
     updateCartQuantity();
