16a37e7f02fb5ac9f31a1843db032b9dfcdac328
Biswajit Samal <biswajitsamalsibu03@gmail.com>
2026-01-30 01:13:28 +0530

    Create order using the backend

--- scripts/checkout/paymentSummary.js
+++ scripts/checkout/paymentSummary.js
@@ -3,6 +3,7 @@ import { getProduct } from "../../data/products.js";
 import { getDeliveryOption } from "../../data/deliveryOption.js";
 import formatCurrency from "../utils/money.js";
 import { updateCartQuantity } from "../utils/cartQuantity.js";
+import { addOrder } from "../../data/order.js";
 export function renderPaymentSummary() {
   let productPriceCents = 0;
   let shippingPriceCents = 0;
@@ -60,12 +61,29 @@ export function renderPaymentSummary() {
             </div>
           </div>
 
-          <button class="place-order-button button-primary">
+          <button class="place-order-button button-primary 
+          js-place-order">
             Place your order
           </button>
     `;
   });
 
   document.querySelector('.js-payment-summary').innerHTML = paymentSummaryHTML;
-  
+
+  document.querySelector('.js-place-order')
+  .addEventListener('click',async ()=>{
+    const response = await fetch('https://supersimplebackend.dev/orders',{
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        cart: cart,
+      })
+    });
+    const order = await response.json();
+    addOrder(order);
+
+    window.location.href = 'orders.html'
+  });
 }
